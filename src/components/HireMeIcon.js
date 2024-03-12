c59f977b65dea73eada3c50a41bc5dd9921543a6
kalkeshwar <kalkeshwaryamsani20@gmail.com>
2024-03-12 13:24:35 +0530

    added hireme mail optionn

--- src/components/HireMeIcon.js
+++ src/components/HireMeIcon.js
@@ -1,14 +1,23 @@
-'use client'
-import React from 'react'
-import { CircularIcon } from './Icon'
+import React from "react";
+import { CircularIcon } from "./Icon";
 
 const HireMeIcon = () => {
-  return (
-    <div className='flex bg-green relative w-[15rem] h-[15rem] overflow-hidden' >
-      <CircularIcon className={'w-full h-auto fill-dark animate-spin-slow'}/>
-      <div className='absolute top-20 left-20 rounded-[50%] bg-black text-white font-medium w-20 h-20 flex justify-center items-center'>HireMe</div>
-    </div>
-  )
-} 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.href = "mailto:kalkeshwaryamsani20@gmail.com";
+  };
 
-export default HireMeIcon
+  return (
+    <div className="flex bg-green relative w-[15rem] h-[15rem] overflow-hidden">
+      <CircularIcon className={"w-full h-auto fill-dark animate-spin-slow"} />
+      <div
+        className="absolute top-20 left-20 rounded-[50%] bg-black text-white font-medium w-20 h-20 flex justify-center items-center cursor-pointer"
+        onClick={handleSubmit}
+      >
+        HireMe
+      </div>
+    </div>
+  );
+};
+
+export default HireMeIcon;
